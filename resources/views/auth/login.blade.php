57b023def67f110a022db21f40066d3368e8e402
ABU TALHA <talhaqc@gmail.com>
2016-11-19 00:44:03 +0600

    login title change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>InfancyIT | Login </title>
+    <title>SUST Society | Login </title>
     {!! Html::style('css/bootstrap.min.css') !!}
     {!! Html::style('font-awesome/css/font-awesome.css') !!}
     {!! Html::style('css/animate.css') !!}
